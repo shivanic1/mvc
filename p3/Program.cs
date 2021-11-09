930406cb0f3a96025dfbc65c5a167d67b7af18b4
shivani <shivanic1.com>
2021-11-09 22:15:52 +0530

    hello world

---
+++ p3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           for( int i = 0; i<5; i++)
+            
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.WriteLine("* ");
+                    Console.ReadLine();
+                }
+                Console.WriteLine("");
+                Console.ReadLine();
+                
+                
+            }
+        }
+    }
+}
